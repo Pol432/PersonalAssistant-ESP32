b9aed6053880afc95b1d3c5b7674b94a1708ed7c
Pol432 <pmrosero@gmail.com>
2023-12-22 22:02:16 -0500

    Connect to BLE

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,116 @@
 #include <Arduino.h>
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// BLE SECTION
+BLEServer *pServer = NULL;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+BLECharacteristic *ssid_characteristic = NULL;
+BLECharacteristic *password_characteristic = NULL;
+
+String ssidValue = "";
+String passwordValue = "";
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
+#define SERVICE_UUID "6bae1e64-1b78-452c-b129-fb9a4152f303"
+
+#define SSID_CHARACTERISTIC_UUID "85694ec2-b42d-4a99-b27b-11d94d7fc582"
+#define PASSWORD_CHARACTERISTIC_UUID "48fc2467-b5ee-4d72-a39f-d31854adbd64"
+
+class MyServerCallbacks : public BLEServerCallbacks
+{
+  void onConnect(BLEServer *pServer)
+  {
+    Serial.println("Connected");
+  };
+
+  void onDisconnect(BLEServer *pServer)
+  {
+    Serial.println("Disconnected");
+  }
+};
+
+class CharacteristicsCallbacks : public BLECharacteristicCallbacks
+{
+  void onWrite(BLECharacteristic *pCharacteristic)
+  {
+    Serial.print("Value Written ");
+    Serial.println(pCharacteristic->getValue().c_str());
+
+    if (pCharacteristic == ssid_characteristic)
+    {
+      ssidValue = pCharacteristic->getValue().c_str();
+      ssid_characteristic->setValue(const_cast<char *>(ssidValue.c_str()));
+      ssid_characteristic->notify();
+    }
+    else if (pCharacteristic == password_characteristic)
+    {
+      passwordValue = pCharacteristic->getValue().c_str();
+      password_characteristic->setValue(const_cast<char *>(passwordValue.c_str()));
+      password_characteristic->notify();
+    }
+  }
+};
+
+void setup()
+{
+  // Initialing serial port
+  Serial.begin(115200);
+
+  // Create the BLE Device
+  BLEDevice::init("BLEExample");
+  // Create the BLE Server
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  delay(100);
+
+  // Create a BLE Characteristic
+  ssid_characteristic = pService->createCharacteristic(
+      SSID_CHARACTERISTIC_UUID,
+      BLECharacteristic::PROPERTY_READ |
+          BLECharacteristic::PROPERTY_WRITE |
+          BLECharacteristic::PROPERTY_NOTIFY |
+          BLECharacteristic::PROPERTY_INDICATE);
+
+  password_characteristic = pService->createCharacteristic(
+      PASSWORD_CHARACTERISTIC_UUID,
+      BLECharacteristic::PROPERTY_READ |
+          BLECharacteristic::PROPERTY_WRITE |
+          BLECharacteristic::PROPERTY_NOTIFY |
+          BLECharacteristic::PROPERTY_INDICATE);
+
+  // Start the BLE service
+  pService->start();
+
+  // Start advertising
+  pServer->getAdvertising()->start();
+
+  /*
+    message_characteristic->setValue("Message one");
+    message_characteristic->setCallbacks(new CharacteristicsCallbacks());
+
+    box_characteristic->setValue("0");
+    box_characteristic->setCallbacks(new CharacteristicsCallbacks());
+   */
+
+  Serial.println("Waiting for a client connection to notify...");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+  /*
+  message_characteristic->setValue("Message one");
+  message_characteristic->notify();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  delay(1000);
+
+  message_characteristic->setValue("Message Two");
+  message_characteristic->notify();
+
+  delay(1000);
+  */
 }
