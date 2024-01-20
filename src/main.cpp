444bd08d544da686caa6fbf3fa80425f4ee53a0a
Pol432 <pmrosero@gmail.com>
2024-01-20 10:40:43 -0500

    First try to connect ESP32 via BLE and write to memory

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,50 @@
 #include <Arduino.h>
 #include "BLEHandler.h"
+#include "StepperMotor.h"
 
 BLEHandler bleHandler;
+StepperMotor stepperMotor;
+
+// Preferences SECTION
+Preferences memory;
+String ssid;
+String password;
 
 void setup()
 {
   Serial.begin(115200);
-  bleHandler.begin();
-  bleHandler.handleBLE();
+  memory.begin("app", false);
+
+  // Getting Wi-Fi info
+  ssid = memory.getString("ssid", "");
+  password = memory.getString("password", "");
+
+  // If ssid or password weren't set before, wait for Bluetooth to set them
+  if (ssid == "" || password == "")
+  {
+    Serial.println("SSID and/or Password not found: Starting BLE");
+    bleHandler.begin();
+
+    // Wait for BLE to set ssid and password
+    while (ssid == "" || password == "")
+    {
+      bleHandler.handleBLE(); // Handle BLE events
+
+      // Check if keys are present before retrieving
+      if (!memory.getUInt("ssid", 0) == 0)
+        ssid = memory.getString("ssid", "");
+
+      if (!memory.getUInt("password", 0) == 0)
+        password = memory.getString("password", "");
+
+      delay(10);
+    }
+
+    Serial.println("Added ssid and password");
+  }
+
+  // Initialize the stepper motor
+  stepperMotor.begin();
 }
 
 void loop()
