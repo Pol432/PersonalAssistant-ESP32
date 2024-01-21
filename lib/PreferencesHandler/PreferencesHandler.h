077bc6eb92412eddc21c7cc64089da5cb3f57a13
Pol432 <pmrosero@gmail.com>
2024-01-20 22:46:51 -0500

    Fixed memory to be global

---
+++ lib/PreferencesHandler/PreferencesHandler.h
@@ -0,0 +1,11 @@
+#ifndef PREFERENCES_HANDLER_H
+#define PREFERENCES_HANDLER_H
+
+#include <Preferences.h>
+
+extern Preferences memory;
+
+void initPreferences();
+void printPreferences();
+
+#endif // PREFERENCES_HANDLER_H
